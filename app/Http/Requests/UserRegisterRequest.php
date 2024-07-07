eb70f4d2cfd184774ac3d7b491996f9d9a9fc6ec
Slymee <jetli_rijan@hotmail.com>
2024-07-07 22:58:40 +0545

    Request file changes

--- app/Http/Requests/UserRegisterRequest.php
+++ app/Http/Requests/UserRegisterRequest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Requests;
+const MAX255 = 'max:255';
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -22,18 +23,18 @@ class UserRegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255', 'bail'],
-            'username' => ['required', 'string', 'max:255', 'unique:users', 'bail'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users', 'bail'],
+            'name' => ['required', 'string', MAX255, 'bail'],
+            'username' => ['required', 'string', MAX255, 'unique:users', 'bail'],
+            'email' => ['required', 'string', 'email', MAX255, 'unique:users', 'bail'],
             'password' => ['required', 'string', 'min:12', 'confirmed', 'bail'],
-            'contact_number' => ['required', 'integer', 'max:255', 'unique:users', 'bail'],
+            'contact_number' => ['required', 'integer', MAX255, 'unique:users', 'bail'],
             'bio' => ['nullable', 'string', 'bail'],
 
-            'street_name' => ['required', 'string', 'max:255', 'bail'],
-            'city_name' => ['required', 'string', 'max:255', 'bail'],
-            'state_province' => ['required', 'string', 'max:255', 'bail'],
-            'postal_code' => ['required', 'integer', 'max:255', 'bail'],
-            'country_name' => ['required', 'string', 'max:255', 'bail'],
+            'street_name' => ['required', 'string', MAX255, 'bail'],
+            'city_name' => ['required', 'string', MAX255, 'bail'],
+            'state_province' => ['required', 'string', MAX255, 'bail'],
+            'postal_code' => ['required', 'integer', MAX255, 'bail'],
+            'country_name' => ['required', 'string', MAX255, 'bail'],
         ];
     }
 
