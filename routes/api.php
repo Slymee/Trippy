e33ff3847d731bdbd58e66cfb8babdc90f0499a0
Slymee <jetli_rijan@hotmail.com>
2024-07-13 02:10:46 +0545

    OAuth Token Test: pass

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/test-api', function (){
+    return \App\Helpers\apiResponse('Test Data', 'It\'s verified', true, 200);
+})->middleware('auth:sanctum');
 
