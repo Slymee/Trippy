dbcf80e663600b452e56110b87e3d90f54595947
Slymee <jetli_rijan@hotmail.com>
2024-07-13 11:39:22 +0545

    Code Reformat

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -1,58 +1,58 @@
 <?php
 
-    namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\API;
 
-    use App\Http\Controllers\Controller;
-    use App\Http\Requests\UserLoginRequest;
-    use App\Http\Requests\UserRegisterRequest;
-    use App\Repositories\Interfaces\UserAuthenticationRepositoryInterface;
-    use App\Services\UserAuthenticationService;
-    use Illuminate\Http\JsonResponse;
-    use Illuminate\Support\Facades\Auth;
-    use function App\Helpers\apiResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserLoginRequest;
+use App\Http\Requests\UserRegisterRequest;
+use App\Repositories\Interfaces\UserAuthenticationRepositoryInterface;
+use App\Services\UserAuthenticationService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use function App\Helpers\apiResponse;
 
-    class UserController extends Controller
+class UserController extends Controller
+{
+    protected $userAuthenticationRepository, $userAuthenticationService;
+    public function __construct(UserAuthenticationRepositoryInterface $userAuthenticationRepository, UserAuthenticationService $userAuthenticationService)
     {
-        protected $userAuthenticationRepository, $userAuthenticationService;
-        public function __construct(UserAuthenticationRepositoryInterface $userAuthenticationRepository, UserAuthenticationService $userAuthenticationService)
-        {
-            $this->userAuthenticationRepository = $userAuthenticationRepository;
-            $this->userAuthenticationService = $userAuthenticationService;
-        }
-        public function register(UserRegisterRequest $request): JsonResponse
-        {
-            try {
-                $user = $this->userAuthenticationRepository->userCreate($request->validated());
+        $this->userAuthenticationRepository = $userAuthenticationRepository;
+        $this->userAuthenticationService = $userAuthenticationService;
+    }
+    public function register(UserRegisterRequest $request): JsonResponse
+    {
+        try {
+            $user = $this->userAuthenticationRepository->userCreate($request->validated());
 
-                $this->userAuthenticationRepository->addressCreate($user->id, $request->validated());
+            $this->userAuthenticationRepository->addressCreate($user->id, $request->validated());
 
-                return apiResponse($user->load('address'), 'User registered successfully', true, 201);
-            }catch (\Exception $e){
-                return apiResponse(null, $e->getMessage(), false, 500);
-            }
-        }
-
-        public function login(UserLoginRequest $request): JsonResponse
-        {
-            try {
-                $credentials = $this->userAuthenticationService->credentials($request->validated());
-
-                if (!Auth::attempt($credentials)) {
-                    return apiResponse(null, 'Invalid credentials', false, 401);
-                }
-
-                $user = Auth::user();
-
-                $accessToken = $this->userAuthenticationService->getAccessToken($user);
-
-                return apiResponse([
-                    'access_token' => $accessToken,
-                    'token_type' => 'Bearer',
-                    'expires_in' => 3600,
-                ], 'Login successful', true, 200);
-
-            }catch (\Exception $e){
-                return apiResponse(null, $e->getMessage(), false, 500);
-            }
+            return apiResponse($user->load('address'), 'User registered successfully', true, 201);
+        }catch (\Exception $e){
+            return apiResponse(null, $e->getMessage(), false, 500);
         }
     }
+
+    public function login(UserLoginRequest $request): JsonResponse
+    {
+        try {
+            $credentials = $this->userAuthenticationService->credentials($request->validated());
+
+            if (!Auth::attempt($credentials)) {
+                return apiResponse(null, 'Invalid credentials', false, 401);
+            }
+
+            $user = Auth::user();
+
+            $accessToken = $this->userAuthenticationService->getAccessToken($user);
+
+            return apiResponse([
+                'access_token' => $accessToken,
+                'token_type' => 'Bearer',
+                'expires_in' => 3600,
+            ], 'Login successful', true, 200);
+
+        }catch (\Exception $e){
+            return apiResponse(null, $e->getMessage(), false, 500);
+        }
+    }
+}
