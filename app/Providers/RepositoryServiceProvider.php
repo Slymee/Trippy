d9b0724301b8ed0132b201919970ac3751d9b8bf
Slymee <jetli_rijan@hotmail.com>
2024-08-13 12:20:33 +0545

    User design repository pattern structure

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -4,10 +4,11 @@ namespace App\Providers;
 
 use App\Repositories\Interfaces\TripRepositoryInterface;
 use App\Repositories\Interfaces\UserAuthenticationRepositoryInterface;
+use App\Repositories\Interfaces\UserRepositoryInterface;
 
 use App\Repositories\TripRepository;
 use App\Repositories\UserAuthenticationRepository;
-
+use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -19,6 +20,7 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         //authentication repository
         $this->app->bind(UserAuthenticationRepositoryInterface::class, UserAuthenticationRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
         $this->app->bind(TripRepositoryInterface::class, TripRepository::class);
     }
 
