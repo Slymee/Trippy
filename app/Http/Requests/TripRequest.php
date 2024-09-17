98313a44a855a8eae680c3e36f7ca08886eb4ab4
Slymee <jetli_rijan@hotmail.com>
2024-09-17 13:43:25 +0545

    time format fix

--- app/Http/Requests/TripRequest.php
+++ app/Http/Requests/TripRequest.php
@@ -28,15 +28,15 @@ class TripRequest extends FormRequest
             'trip_price' => ['required', 'numeric'],
             'start_date' => ['required', 'date_format:Y-m-d'],
             'end_date' => ['required', 'date_format:Y-m-d'],
-            'arrival_time' => ['required', 'date_format:H:i'],
+            'arrival_time' => ['required', 'string', 'max:255'],
             'means_of_transport' => ['required', 'string', 'max:255'],
             'is_private' => ['sometimes', 'boolean'],
-        
+
             'start_loc' => ['required', 'string', 'max:255'],
             'start_loc_name' => ['required', 'string', 'max:255'],
             'end_loc' => ['required', 'string', 'max:255'],
             'end_loc_name' => ['required', 'string', 'max:255'],
-        
+
             'location' => ['sometimes', 'array'],
             'location.*' => ['sometimes', 'string', 'max:255'],
         ];
