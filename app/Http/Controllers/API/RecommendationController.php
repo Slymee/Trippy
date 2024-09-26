db83138855a9ef6063db76fa54b28512dfdd332c
Rijan <jetli_rijan@hotmail.com>
2024-09-26 14:13:18 +0545

    Added controller for recommendation: complete

--- app/Http/Controllers/API/RecommendationController.php
+++ app/Http/Controllers/API/RecommendationController.php
@@ -3,9 +3,38 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Trip;
+use App\Services\RecommendationService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+use function App\Helpers\apiResponse;
 
 class RecommendationController extends Controller
 {
-    //
+    public $recommendationService;
+
+    public function __construct(RecommendationService $recommendationService)
+    {
+        $this->recommendationService = $recommendationService;
+    }
+
+    public function index($tripId)
+    {
+        try{
+            $trip = Trip::findOrFail($tripId);
+
+            $recommendedTrips = $this->recommendationService->recommend($trip);
+
+            if($recommendedTrips){
+                return apiResponse($recommendedTrips , 'Similar trips found.', true, 200);
+            }
+
+            return apiResponse(null, 'Similar Trips not found.', false, 404);
+        }catch(\Exception $e){
+            Log::error('Caught Exception: '. $e->getMessage());
+            Log::error('Exception Details: '. $e);
+            return apiResponse(null, $e->getMessage(), false, 500);
+        }
+    }
 }
