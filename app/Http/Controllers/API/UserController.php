efc642abcc86fb522bdc49ebb59a8914a6aeb179
Slymee <jetli_rijan@hotmail.com>
2024-09-29 19:11:28 +0545

    update: api request change to patch mode

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\UserRegisterRequest;
 use App\Repositories\Interfaces\UserRepositoryInterface;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rule;
 
 use function App\Helpers\apiResponse;
 
@@ -36,14 +37,18 @@ class UserController extends Controller
         }
     }
 
-    public function update(UserRegisterRequest $request, $userId)
+    public function update(Request $request, $userId)
     {
         try{
             if(!$this->userRepository->getUserData($userId)){
                 return apiResponse(null, 'User not Found.', false, 404);
             }
 
-            $userData = $this->userRepository->updateUserData($userId, $request->validated());
+            // Validate the request data
+            $validatedData = $request->validate($this->updateRules($userId));
+
+            // Update the user data
+            $userData = $this->userRepository->updateUserData($userId, $validatedData);
 
             return apiResponse($userData, 'Successfully Updated.', true, 200);
         }catch (\Exception $e){
@@ -52,5 +57,32 @@ class UserController extends Controller
 
             return apiResponse(null, $e->getMessage(), false, 500);
         }
+    }
+
+
+
+    protected function updateRules($userId)
+    {
+        return [
+            'name' => ['sometimes', 'string', 'max:255', 'bail'],
+            'username' => [
+                'sometimes', 'string', 'max:255',
+                Rule::unique('users')->ignore($userId), // Ignore current user's username
+                'bail'
+            ],
+            'email' => [
+                'sometimes', 'string', 'email', 'max:255',
+                Rule::unique('users')->ignore($userId), // Ignore current user's email
+                'bail'
+            ],
+            'password' => ['sometimes', 'string', 'min:5', 'confirmed', 'bail'],
+            'contact' => [
+                'sometimes', 'integer',
+                Rule::unique('users')->ignore($userId), // Ignore current user's contact
+                'bail'
+            ],
+            'address' => ['sometimes', 'string', 'max:255', 'bail'],
+            'bio' => ['sometimes', 'string', 'bail'],
+        ];
     }
 }
